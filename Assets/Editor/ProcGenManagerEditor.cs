3cdce452f7dabc214b86748a5330fc98b0f7f738
bapt2 <baptiste18c.touraud@gmail.com>
2024-04-06 19:04:23 +0200

    adding Editor script

---
+++ Assets/Editor/ProcGenManagerEditor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using Unity.EditorCoroutines.Editor;
+
+[CustomEditor(typeof(ProcGenManager))]
+public class ProcGenManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Regenerate Texture"))
+        {
+            ProcGenManager targetManager = serializedObject.targetObject as ProcGenManager;
+            targetManager.RegenerateTextures();
+        }
+
+        if (GUILayout.Button("Regenerate Detail Prototypes"))
+        {
+            ProcGenManager targetManager = serializedObject.targetObject as ProcGenManager;
+            targetManager.RegenerateDetailPrototypes();
+        }
+
+        if (GUILayout.Button("Regenerate World"))
+        {
+            ProcGenManager targetManager = serializedObject.targetObject as ProcGenManager;
+            EditorCoroutineUtility.StartCoroutine(PerformRegeneration(targetManager), this);
+        }
+    }
+
+    int progressID;
+    IEnumerator PerformRegeneration(ProcGenManager targetManager)
+    {
+        progressID = Progress.Start("Regenerating terrain");
+        yield return targetManager.AsyncRegenerateWorld(OnStatusReported);
+
+        Progress.Remove(progressID);
+
+        yield return null;
+    }
+
+    void OnStatusReported(EGenerationStage currentStage, string status)
+    {
+        Progress.Report(progressID, (int)currentStage, (int)EGenerationStage.NumStage, status);
+    }
+}
